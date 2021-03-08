d7ecd70822e7998b149266e762e33c2689e6e01f
iriewiew <annoi@live.com>
2021-03-08 17:11:39 +0700

    add fix2

--- next.config.js
+++ next.config.js
@@ -1,7 +1,25 @@
+const path = require('path')
+const debug = process.env.NODE_ENV !== 'production'
 module.exports = {
+  distDir: 'build',
+  // basePath: '/next-contentful/out',
+  assetPrefix: !debug ? '/con_next/' : '',
+  // assetPrefix: '/next-contentful/out/',
   env: {
-    REACT_APP_CONTENTFUL_SPACE_ID: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
-    REACT_APP_CONTENTFUL_ACCESS_TOKEN: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN,
+    REACT_APP_CONTENTFUL_SPACE_ID: process.env.REACT_APP_CONTENTFUL_SPACE_ID || process.env.CONTENTFUL_SPACE_ID,
+    REACT_APP_CONTENTFUL_ACCESS_TOKEN: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN || process.env.CONTENTFUL_ACCESS_TOKEN,
   },
-  assetPrefix: '/'
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+    }
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  // reactStrictMode: true,
+  trailingSlash: true,
 };
