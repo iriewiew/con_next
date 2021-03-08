34e73a815defca1406b3699257bd5be0eb3f769c
iriewiew <annoi@live.com>
2021-03-08 16:42:56 +0700

    fix url

--- next.config.js
+++ next.config.js
@@ -3,5 +3,5 @@ module.exports = {
     REACT_APP_CONTENTFUL_SPACE_ID: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
     REACT_APP_CONTENTFUL_ACCESS_TOKEN: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN,
   },
-  assetPrefix: '/next-contentful'
+  assetPrefix: '/con_next'
 };
